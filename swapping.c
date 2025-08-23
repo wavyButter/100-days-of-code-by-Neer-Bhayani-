9e58f06d8c2997b96ce1cd72055b6cda14f88ff3
Neer Bhayani <bhayanineer@gmail.com>
2025-08-23 15:29:01 +0530

    Update swapping.c

--- swapping.c
+++ swapping.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+int main() {
+    int a, b, temp;
+
+    
+    printf("enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("before swapping: a = %d, b = %d\n", a, b);
+
+    
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("after swapping: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
 
