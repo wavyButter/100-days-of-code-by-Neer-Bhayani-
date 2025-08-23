ddd7d30a9a546d13f18792e8af89a0ab359385d0
Neer Bhayani <bhayanineer@gmail.com>
2025-08-23 14:46:12 +0530

    Update temperature.c

--- temperature.c
+++ temperature.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main() {
+    float celcius, fahrenheit;
+    printf("Write temperature in Celsius: ");
+    scanf("%f", &celcius);
+    
+    fahrenheit = (celcius * 9 / 5) + 32;
+    printf("%.2f Celsius = %.2f Fahrenheit\n", celcius, fahrenheit);
+
+    return 0;
+}
