6657f0f05c67db877794a6f6ba227b5e19d4d1fc
Neer Bhayani <bhayanineer@gmail.com>
2025-09-15 19:34:18 +0530

    Create lcm.c

---
+++ lcm.c
@@ -0,0 +1,19 @@
+// Write a program to find the LCM of two numbers.
+#include <stdio.h>
+
+#include <stdio.h>
+
+int main() {
+    int a, b, gcd, lcm, i;
+    scanf("%d %d", &a, &b);
+
+
+    for(i = 1; i <= a && i <= b; i++) {
+        if(a % i == 0 && b % i == 0)
+            gcd = i;
+    }
+    lcm = (a * b) / gcd;
+
+    printf("%d\n", lcm);
+    return 0;
+}
