780139cb28fc6c56e2fdef67d32b80eb669e9ee0
Neer Bhayani <bhayanineer@gmail.com>
2025-11-27 00:18:41 +0530

    Create pattern2.c

---
+++ pattern2.c
@@ -0,0 +1,18 @@
+//Write a program to print the following pattern:
+//*
+//**
+//***
+//****
+//*****
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
