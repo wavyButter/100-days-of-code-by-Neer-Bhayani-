32241261953adbe774941b75476da7182899ad22
Neer Bhayani <bhayanineer@gmail.com>
2025-12-02 02:15:02 +0530

    Create 131.c

---
+++ 131.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    enum Days {SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY};
+    printf("SUNDAY = %d\n", SUNDAY);
+    printf("MONDAY = %d\n", MONDAY);
+    printf("TUESDAY = %d\n", TUESDAY);
+    printf("WEDNESDAY = %d\n", WEDNESDAY);
+    printf("THURSDAY = %d\n", THURSDAY);
+    printf("FRIDAY = %d\n", FRIDAY);
+    printf("SATURDAY = %d\n", SATURDAY);
+    return 0;
+}
