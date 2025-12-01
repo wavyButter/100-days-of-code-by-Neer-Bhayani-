6d68204b9472eca4809424771017893dc90b269a
Neer Bhayani <bhayanineer@gmail.com>
2025-12-01 22:08:29 +0530

    Create 113.c

---
+++ 113.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+    int arr[100];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    
+    printf("%d\n", arr[k - 1]);
+    return 0;
+}
