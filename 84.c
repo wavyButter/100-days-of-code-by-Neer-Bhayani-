6bad84951be1acb1227ad0b328abb2b5ed6c07a0
Neer Bhayani <bhayanineer@gmail.com>
2025-12-01 13:01:59 +0530

    Create 84.c

---
+++ 84.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+    
+    printf("Enter lowercase string: ");
+    fgets(str, sizeof(str), stdin);
+    
+    while(str[i] != '\0' && str[i] != '\n') {
+        if(str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;
+        }
+        i++;
+    }
+    
+    printf("Uppercase string: %s", str);
+    
+    return 0;
+}
