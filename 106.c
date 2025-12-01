4ef9b270eeea93045f5db9271c96bca156b3ec3f
Neer Bhayani <bhayanineer@gmail.com>
2025-12-01 22:02:25 +0530

    Create 106.c

---
+++ 106.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[100];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    for (int i = 0; i < n; i++) {
+        int found = 0;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[i]) {
+                printf("%d", arr[j]);
+                found = 1;
+                break;
+            }
+        }
+        if (!found) {
+            printf("-1");
+        }
+        if (i < n - 1) {
+            printf(",");
+        }
+    }
+    printf("\n");
+    return 0;
+}
